834d4499d8896849f83593b9cb7ec0a9a6b7949f
Your Name <your-email@example.com>
2025-02-07 20:42:22 +0800

    fix: 优化游戏结束弹窗文案
    1. 修改提问者找到真人时的文案：'恭喜！在第X轮找到了真人玩家！'
    2. 修改回答者被找到时的文案：'可惜，在第X轮被发现了'
    3. 统一弹窗标题为'游戏结束'

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -358,11 +358,11 @@ const setupSocketListeners = () => {
 
 const handleGameOver = (message) => {
   showGameOverDialog.value = true
-  gameOverTitle.value = isQuestioner.value ? '提问者游戏结束' : '回答者游戏结束'
+  gameOverTitle.value = isQuestioner.value ? '游戏结束' : '游戏结束'
   
   if (isQuestioner.value) {
     if (message.includes('选中了真人')) {
-      gameOverMessage.value = `恭喜，第${currentRound.value}轮就找到了真人玩家！`
+      gameOverMessage.value = `恭喜！在第${currentRound.value}轮找到了真人玩家！`
     } else if (message.includes('所有AI')) {
       gameOverMessage.value = '所有AI玩家都被找出来了，但真人玩家成功存活到最后！'
     } else {
@@ -370,7 +370,7 @@ const handleGameOver = (message) => {
     }
   } else {
     if (message.includes('选中了真人')) {
-      gameOverMessage.value = `可惜，止步于第${currentRound.value}轮`
+      gameOverMessage.value = `可惜，在第${currentRound.value}轮被发现了`
     } else if (message.includes('所有AI')) {
       gameOverMessage.value = '恭喜你！所有AI都被找出来了，你成功存活到最后！'
     } else {
